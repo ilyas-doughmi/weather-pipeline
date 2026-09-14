cc9a4d51bd7426b33cbab9878df44929ad52bdbd
Doughmi Ilyas <188033398+ilyas-doughmi@users.noreply.github.com>
2026-09-14 14:06:22 +0100

    feat: adding new methode that take the lat & lon and it return full link to see all the info from open meteo API

--- extraction/extract_weather.py
+++ extraction/extract_weather.py
@@ -1,9 +1,12 @@
 import pandas as pd
 
-
 def readCSV(path):
     df = pd.read_csv(path,encoding="utf-8")
     return df[["city","lat","lon"]]
 
-if __name__ == "__main__":
-    print(readCSV("data/ma-cities.csv"))
+def build_url(lat,lon):
+    baseUrl = "https://api.open-meteo.com/v1/forecast"
+    return baseUrl + f"?latitude={lat}&longitude={lon}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,precipitation_probability_max,wind_speed_10m_max,wind_gusts_10m_max,weather_code&timezone=auto&forecast_days=3"
+
+if __name__ == "__main__":  
+    print(build_url(52.52,13.41))
