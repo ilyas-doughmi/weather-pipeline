f952df78dde8ff4ba7b28de23031d00c4ff48588
Doughmi Ilyas <188033398+ilyas-doughmi@users.noreply.github.com>
2026-09-14 14:57:50 +0100

    feat: adding main function that will use the readCSV method to read the CSV file and then looping through all columns that we have and extracting data to a payload then write full data into a json file that will have cityname.json and make it in the bronze directory

--- extraction/extract_weather.py
+++ extraction/extract_weather.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import json
+import requests as req
 
 def readCSV(path):
     df = pd.read_csv(path,encoding="utf-8")
@@ -8,5 +10,25 @@ def build_url(lat,lon):
     baseUrl = "https://api.open-meteo.com/v1/forecast"
     return baseUrl + f"?latitude={lat}&longitude={lon}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,precipitation_probability_max,wind_speed_10m_max,wind_gusts_10m_max,weather_code&timezone=auto&forecast_days=3"
 
+def main():
+    df = readCSV("data/ma-cities.csv")
+    for data in df.itertuples():
+        print(f"extracting {data.city}....")
+        lat,lon = data.lat,data.lon
+        url = build_url(lat,lon)
+        response = req.get(url).json()
+
+        payload = {
+            "city": data.city,
+            "lat" : data.lat,
+            "lon" : data.lon,
+            "response" : response
+        }
+    
+        with open(f"bronze/{data.city}.json", "w") as f:
+            print(f"creating json file for  {data.city}....")
+            json.dump(payload,f)
+        print(f"Done {data.city}")
+
 if __name__ == "__main__":  
-    print(build_url(52.52,13.41))
+    main()
