c1388168cbde6bc094377b05af0c8fbfad0db8ce
Doughmi Ilyas <188033398+ilyas-doughmi@users.noreply.github.com>
2026-09-15 12:09:55 +0100

    feat: adding new methode that will take the path and the files lists and it will loop through those files and first switch them into data frames then append them to a list after that concat it to new varialbe that will after that export it to csv file that will be located into the silver folder

--- transformation/transform_weather.py
+++ transformation/transform_weather.py
@@ -1,5 +1,6 @@
 import os
-
+import json
+import pandas as pd
 
 def bronzedata(path):
     jsonFile = []
@@ -9,6 +10,22 @@ def bronzedata(path):
 
     return jsonFile
 
+def buildrows(path,files):
+    frames = []
+
+    for file in files:
+        with open(f"{path}/{file}","r") as f:
+            data =json.load(f)
+            daily = data["response"]["daily"]
+            df = pd.DataFrame([daily])
+            df["city"] = data["city"]
+            df["latitude"] = data["lat"]
+            df["longitude"] = data["lon"]
+            df["fetched_at"] = data["fetched_at"]
+            frames.append(df) 
+    combine = pd.concat(frames,ignore_index=False)
+    combine = combine.explode(["time","temperature_2m_max","temperature_2m_min","precipitation_sum","precipitation_probability_max","wind_speed_10m_max","wind_gusts_10m_max","weather_code"])
+    combine.to_csv("silver/data.csv", index=True)
 
 if __name__ ==  "__main__":
-   print( bronzedata("bronze"))
+   print( buildrows("bronze",bronzedata("bronze")))
